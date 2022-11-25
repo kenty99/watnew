8cbb94170ee7011198cef8a2b3dfd658c225f232
澤木柊斗 <sawakishuto@sawakihiiragitonoMacBook-Pro.local>
2022-11-25 23:50:30 +0900

    お試し

---
+++ gacha.js
@@ -0,0 +1,44 @@
+'usestrict';
+const lang = ["Java","Javascript","C言語","PHP","Ruby","Perl","Swift"]
+let gaccha = ()=>{
+ var random = Math.floor(Math.random()*12)
+ if (random <= 6){
+  
+    document.getElementById('list').innerText = `${lang[random]}を習得した！`
+    
+    }
+else{
+    document.getElementById('list').innerText = "何も習得することができなかった..."
+    }
+
+
+
+
+if (random == 0){
+    
+    document.getElementById('star1').innerText = "★"
+
+}
+else if (random == 1){
+    document.getElementById('star2').innerText = "★"
+}
+else if (random == 2){
+    document.getElementById('star3').innerText = "★"
+}
+else if (random == 3){
+    document.getElementById('star4').innerText = "★"
+}
+else if (random == 4){
+    document.getElementById('star5').innerText = "★"
+}
+else if (random == 5){
+    document.getElementById('star6').innerText = "★"
+}
+else if (random == 6){
+    document.getElementById('star7').innerText = "★"
+}
+
+
+};
+let elementv= document.getElementById('gacha')
+addEventListener('click',gaccha)
