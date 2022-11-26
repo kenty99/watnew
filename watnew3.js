893236bf49d56c3aa45d031106d296acd1b59194
えぼるど <ryoma@eborudoru.local>
2022-11-26 13:50:12 +0900

    更新

--- watnew3.js
+++ watnew3.js
@@ -7,7 +7,7 @@ for(var i = 0; i <= 30; i++){
     var gengo = readline. question("使用言語を書け");
     
     if(gengo !== ""){
-        a++;
+        ++a;
         if(a == 10){
             console.log("中級者です");
         }else if(a == 20){
