fd50208569920ddc42b3248bf8c4273104ac55cb
澤木柊斗 <sawakishuto@sawakihiiragitonoMacBook-Pro.local>
2022-11-26 18:20:51 +0900

    更新

--- gacha.js
+++ gacha.js
@@ -7,18 +7,30 @@ let lang4 = 0
 let lang5 = 0
 let lang6 = 0
 let lang7 = 0
+let lang8 = 0
+let lang9 = 0
+let lang10 = 0
+let lang11 = 0
+let lang12 = 0
+let lang13 = 0
+let lang14 = 0
+let lang15 = 0
+let lang16 = 0
+let lang17 = 0
+let lang18 = 0
+let lang19 = 0
+let lang20 = 0
+
+let n = 0
 
 
-langlist = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-langlist.forEach({
-const lang[langlist] = 0 
-})
-const lang = ["Java","Javascript","C言語","PHP","Ruby","Perl","Swift"]
+
+const lang = ["Java","Javascript","C言語","PHP","Ruby","Perl","Swift","C++","C#","SQL","VBA","Scala","Lua","COBOl","Object-C","Kotolin","R","TypeScript","Go","Visual Basic"]
 let gaccha = ()=>{
-if (language.length != 7){
- var random = Math.floor(Math.random()*12)
+if (language.length != 20){
+ var random = Math.floor(Math.random()*50)
  
- if (random <= 6){
+ if (random <= 20){
   
     document.getElementById('list').innerText = `${lang[random]}を習得した！`
     
@@ -33,7 +45,7 @@ else{
 
 if (random == 0 && lang1 == 0){
     language[language.length]="1"
-    document.getElementById(`'star${}`).innerText = "★"
+    document.getElementById('star1').innerText = "★"
     lang1 = 1
     
 }
@@ -99,103 +111,70 @@ else if (random == 9 && lang10 == 0){
 }
 else if (random == 10 && lang11 == 0){
     language[language.length]="1"
-    document.getElementById('star').innerText = "★"
+    document.getElementById('star11').innerText = "★"
     lang11 = 1
     
 }
 else if (random == 11 && lang12 == 0){
     language[language.length]="1"
-    document.getElementById('star6').innerText = "★"
-    lang6 = 1
+    document.getElementById('star12').innerText = "★"
+    lang12 = 1
     
     
 }
 else if (random == 12 && lang13 == 0){
     language[language.length]="1"
-    document.getElementById('star').innerText = "★"
-    lang7 = 1
+    document.getElementById('star13').innerText = "★"
+    lang13 = 1
     
     
 }
 else if (random == 13 && lang14 == 0){
     language[language.length]="1"
-    document.getElementById('star2').innerText = "★"
-    lang2 = 1
+    document.getElementById('star14').innerText = "★"
+    lang14 = 1
     
   
 }
 else if (random == 14 && lang15 == 0){
     language[language.length]="1"
-    document.getElementById('star3').innerText = "★"
-    lang3 = 1
+    document.getElementById('star15').innerText = "★"
+    lang15 = 1
     
     
 }
 else if (random == 15 && lang16 == 0){
     language[language.length]="1"
-    document.getElementById('star4').innerText = "★"
-    lang4 = 1
+    document.getElementById('star16').innerText = "★"
+    lang16 = 1
     
 }
 else if (random == 16 && lang17 == 0){
     language[language.length]="1"
-    document.getElementById('star5').innerText = "★"
-    lang5 = 1
+    document.getElementById('star17').innerText = "★"
+    lang17 = 1
     
 }
 else if (random == 17 && lang18 == 0){
     language[language.length]="1"
-    document.getElementById('star6').innerText = "★"
-    lang6 = 1
+    document.getElementById('star18').innerText = "★"
+    lang18 = 1
     
     
 }
 else if (random == 18 && lang19 == 0){
     language[language.length]="1"
-    document.getElementById('star7').innerText = "★"
-    lang7 = 1
+    document.getElementById('star19').innerText = "★"
+    lang19 = 1
     
     
 }
 else if (random == 19 && lang20 == 0){
     language[language.length]="1"
-    document.getElementById('star2').innerText = "★"
-    lang2 = 1
+    document.getElementById('star20').innerText = "★"
+    lang20 = 1
     
   
-}
-else if (random == 20 && lang21 == 0){
-    language[language.length]="1"
-    document.getElementById('star3').innerText = "★"
-    lang3 = 1
-    
-    
-}
-else if (random == 21 && lang22 == 0){
-    language[language.length]="1"
-    document.getElementById('star4').innerText = "★"
-    lang4 = 1
-    
-}
-else if (random == 22 && lang23 == 0){
-    language[language.length]="1"
-    document.getElementById('star5').innerText = "★"
-    lang5 = 1
-    
-}
-else if (random == 23 && lang24 == 0){
-    language[language.length]="1"
-    document.getElementById('star6').innerText = "★"
-    lang6 = 1
-    
-    
-}
-else if (random == 24 && lang25 == 0){
-    language[language.length]="1"
-    document.getElementById('star7').innerText = "★"
-    lang7 = 1
-    
-    
 }
 
 
