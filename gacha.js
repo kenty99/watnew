12509259294c07f231f9103a8071f16fd451cdc5
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2023-03-01 19:40:16 +0900

    gamensenni

--- gacha.js
+++ gacha.js
@@ -8,7 +8,13 @@ $(window).on('load',function(){
         $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
   
     });
-    //=====ここまで背景が伸びた後に動かしたいJSをまとめる
+    //=
+
+ const isshow = document.getElementById('botan')
+ isshow.addEventListener('click',function(){
+    console.log("kutikjku")
+    $('.tiping').toggleClass('isshow')
+});
         
 });
 let language = []
@@ -43,7 +49,6 @@ if (language.length != 20){
  var random = Math.floor(Math.random()*23)
  
  if (random <= 19){
-  
     document.getElementById('list').innerText = `${lang[random]}を習得した！`
     
     }
@@ -235,6 +240,89 @@ $('.button').on('click',() => {
     setTimeout(()=>{document.getElementById('gif').src= ''},1300)
     
  })
+
+ var textList = [
+    'Hello World',
+    'Good',
+    'I love JavaScript',
+    'This is MyApp',
+    'Welcome'
+  ];
+   
+  const typeDisplay = document.getElementById("typedisplay");
+  const typeInput = document.getElementById("type-input");
+  const timer = document.getElementById("timer");
+  
+  
+  /*生誤判定*/
+  typeInput.addEventListener("input", () => {
+      const sentenceArray = typeDisplay.querySelectorAll("span");
+      // console.log(sentenceArray);
+      const arrayValue = typeInput.value.split("");
+      // console.log(arrayValue);
+      let correct = true;
+      sentenceArray.forEach((characterSpan, index) => {
+          if((arrayValue[index] == null)){
+              characterSpan.classList.remove("correct");
+              characterSpan.classList.remove("incorrect");
+              correct = false;
+          }
+          else if(characterSpan.innerText == arrayValue[index]){
+              characterSpan.classList.add("correct");
+              characterSpan.classList.remove("incorrect");
+          }else{
+              characterSpan.classList.add("incorrect");
+              characterSpan.classList.remove("correct");
+              correct = false;
+          }
+      });
+  
+      if(correct == true){
+          RenderNextSentence();
+      }
+  }); 
+  
+  function RenderNextSentence(){
+      const sentence = textList[Math.floor(Math.random()*textList.length)];
+      typeDisplay.innerText = "";
+  
+      let onetext = sentence.split("");
+      // console.log(onetext);
+      onetext.forEach((character) => {
+          const characterSpan = document.createElement("span");
+          characterSpan.innerText = character;
+          // console.log(characterSpan);
+          typeDisplay.appendChild(characterSpan);
+          // characterSpan.classList.add("correct");
+      });
+  
+      /* */
+      typeInput.value = "";
+  
+      StartTimer();
+  }
+  
+  let startTime;
+  let origineTime = 10;
+  function StartTimer(){
+      timer.innerText = origineTime;
+      startTime = new Date();
+      setInterval(() => {
+          timer.innerText = origineTime - getTimerTime();
+          if(timer.innerText <= 0) TimeUp();
+      }, 1000);
+  }
+  
+  function getTimerTime(){
+      return Math.floor( (new Date() - startTime) / 1000);
+  }
+  
+  function TimeUp(){
+      RenderNextSentence();
+  }
+  
+  RenderNextSentence();
+
 
 
 
